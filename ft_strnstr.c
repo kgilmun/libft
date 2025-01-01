dd4049d82d76ea34ed8e4d491d688c2648ddb5e4
kgilmun <kevingmo96@gmail.com>
2025-01-01 13:29:15 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,28 +1,20 @@
 char *strnstr(const char *haystack, const char *needle, size_t len) {
   size_t i = 0;
   int j = 0;
-  //NO MOVER LAS DIRECCIONES, QUITAR TODOS LOS ASTERISCOS Y CAMVIARLO POR ÍNDICES
   if (*needle == '\0')
-    return (haystack)
-    return
+    return (haystack);
   while (i < len && *haystack) {
-    while (i < len && *haystack == needle[j]) {
+    j = 0;
+    while (i + j < len && haystack[j] == needle[j])
       j++;
-      ¿i++;?
-    }
     if (needle[j] == '\0')
-      return (needle);
-    else
-      j = 0;
-  //CREO QUE TENGO QUE HACER OTRO CONTROL PARA QUE NO ME SUME LA i 2 VECES EN CASO DE QUE LAS LETRAS HAYAN SIDO IGUAL Y DESPUES DIFERENTES
+      return (haystack);
+    else if (i + j == len)
+      return (NULL);
+    else {
+      i++;
+      haystack++;
+    }
   }
   return (NULL);
 }
-
-//QUE TAL ALGO TIPO:
-while (haystack[i + j] == needle[j])
-  j++;
-if (needle[j] == '\0')
-      return (needle);
-else
-      j = 0;
