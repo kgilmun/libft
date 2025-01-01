a041fabf91647b26889a917e3726a5a1b3f0d44e
kgilmun <kevingmo96@gmail.com>
2025-01-01 13:46:01 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,18 +3,16 @@ char *strnstr(const char *haystack, const char *needle, size_t len) {
   size_t j = 0;
   if (*needle == '\0')
     return (haystack);
-  while (i < len && *haystack) {
+  while (i < len && haystack[i]) {
     j = 0;
     while (i + j < len && haystack[j] == needle[j])
       j++;
     if (needle[j] == '\0')
-      return (haystack);
+      return (haystack + i);
     else if (i + j == len)
       return (NULL);
-    else {
+    else
       i++;
-      haystack++;
-    }
   }
   return (NULL);
 }
