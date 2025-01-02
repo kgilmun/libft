f103ab023753ca3ca5e666dc64b2b9a625d67565
kgilmun <kevingmo96@gmail.com>
2025-01-02 17:35:54 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,11 +3,10 @@ int memcmp(const void *ptr1, const void *ptr2, size_t n) {
   const unsigned char *p2 = (const unsigned char *)ptr2;
   size_t i = 0;
 
-  while (i < n && (*p1 && *p2) {
-    if (*p1 != *p2)
-      return (*p1 - *p2);
-    p1++;
-    p2++;
+  while (i < n) {
+    if (p1[i] != p2[i])
+      return (p1[i] - p2[i]);
+    i++;
   }
-  return (*p1 - *p2);
+  return (0);
 }
