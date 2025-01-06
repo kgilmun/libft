93a4aa977fd54dbb1bce98846e60c20640f370a7
kgilmun <kevingmo96@gmail.com>
2025-01-06 12:33:56 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,12 +1,23 @@
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-  unsigned int  i;
+  unsigned int  len, i;
+  char *result;
 
-  i = 0;
+  if ( s == NULL || f == NULL)
+    return (NULL);
+
+  len = 0;
   while (s[i] != '\0')
+    len++;
+  result = (char *)malloc((len + 1) * sizeof(char));
+  if (result == NULL)
+    return (NULL);
+  i = 0;
+  while (i < len)
   {
-    s[i] = f(i, s);
+    result[i] = f(i, s[i]);
     i++;
   }
-  return (s);
+  result[i] = '\0';
+  return (result);
 }
