fa8708246179531e255f31126bb0317f7777ad22
kgilmun <kevingmo96@gmail.com>
2025-01-06 09:45:08 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -41,44 +41,35 @@ static	int	ft_countwords(const char *s, char c)
 	return (count);
 }
 
-//This function should return de initial letter for each word between separator
-// static	int	ft_initialwordletter (const char *s, int *i, char c)
-// {
-// 	int	initial;
-// 	while (s[*i] != '\0')
-// 	{
-// 		if (*i == 0 && s[*i] != c)
-// 		{
-// 			initial = *i;
-// 			i++;
-// 			return (initial);
-// 		}
-// 		if (s[*i - 1] == c && s[*i] != c)
-// 		{
-// 			initial = *i;
-// 			i++;
-// 			return (initial);
-// 		}
-// 		i++;
-// 	}
-// }
+char	*ft_substr(const char *str, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	unsigned int	count;	
+	char			*p;
 
-// static	int	ft_finalwordletter(const char *s, int *j, char c)
-// {
-// 	int	final;
-	
-// 	while (s[*j] != '\0')
-// 	{
-// 		if (s[*j - 1] != c && s[*j] == c)
-// 		{
-// 			final = *j;
-// 			j++;
-// 			return (final);
-// 		}
-// 	}
-// }
+	count = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[count])
+		count++;
+	if (start >= count)
+		return (NULL);
+	if ((start + len) > count)
+		len = count - start;
+	i = 0;
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		p[i] = str[start + i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
 
-static	void	storewords(const char *s, char c)
+static	char	**ft_storewords(const char *s, char c, char **arr)
 {
 	int	i, start, end, n;
 	char	*p;
@@ -89,50 +80,55 @@ static	void	storewords(const char *s, char c)
 	{
 		if (s[i] != c)
 		{
+		    printf("\nsoy diferente de c\n en la posicion inicial es: %i\n", i);
 			start = i;
-			while (s[i] != 0)
-				i++;
+			while (s[i] != c)
+			{
+			    printf("soy diferente de c en la posicion %i\n", i);
+			    i++;
+			} 
+			printf("la posicion final es %i\n\n", i);
 			end = i;
-			p = malloc(((end - start) + 1) * sizeof(char));
+			p = ft_substr (s, start, (end - start));
 			if (p == NULL)
-				return (NULL);
-			p = strlcpy(p, s, (end - start));
+			{
+			    return NULL;
+			}
+			printf("he copiado la palabra: '%s' en el puntero p\n", p);
 			arr[n] = p;
+			printf("se ha añadido '%s' al array\n", p);
 			n++;
 		}
-		else
+		if (s[i] == c)
 			i++;
 	}
 	arr[n] = NULL;
+	return (arr);
+	
 }
 
 char	**ft_split(char const *s, char c)
 {
 	int	numwords;
 	char	**arr;
-	int	i;
-	int	j;
-	int	n;
-	//int	arrlen;
 
 	
 	numwords = ft_countwords(s, c);
 	arr = malloc((numwords + 1) * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
-		*arr = "camello";
-	i = 0;
-	j = 0;
-	n = 0;
-	//arrlen = ft_strlen(arr);
-	while (s[i])
-	{
-		printf("el caracter inicial es %c y el caracter final es %c \n", s[ft_initialwordletter(s, &i, c)] , s[ft_finalwordletter(s, &j, c)]);
-	}
-	return **arr;
+	arr = ft_storewords(s, c, arr);
+	return arr;
 }
 
 int main () {
-const char *s = "   Hola  Como    estas  ";
-	ft_split(s, ' ');
+    const char *s = "   Hola  Como    estas  ";
+	char **p = ft_split(s, ' ');
+	int i = 0;
+	printf("Se tiene que imprimir por pantalla: '%s' haciendo split:\n\n", s);
+	while (p[i] != NULL) {
+	    printf("%s\n", p[i]);
+	    i++;
+	}
+	return 0;
 }
